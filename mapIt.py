b3e09d671147cc9b04ebb1a025274b4ec09329c7
Ashley Bunch <bunchley007@gmail.com>
2024-10-04 21:05:52 -0600

    grab address from commandline

--- mapIt.py
+++ mapIt.py
@@ -1,3 +1,17 @@
-import webbrowser
+#! python3 
+# mapIt.py - Launches a map in the browser using an address from the 
+# command line or clipboard.
+
+
+
+import webbrowser, sys
+
+if len(sys.argv) > 1:
+    #Get address from command line. 
+    address = ' '.join(sys.argv[1:])
+
+# TODO: Get Address from clipboard
+
+
 webbrowser.open('http://inventwithpython.com/')
 
